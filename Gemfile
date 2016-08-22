e6ff1ff2dfb266f224d0220397a4fbe1b42d9be5
Erik <erikwjonsson@gmail.com>
2016-08-22 18:28:46 +0200

    Tried some Rspec with Capybara. Would not work when submitting form, giving Format:unknown error.

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-# gem 'therubyracer', platforms: :ruby
+gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails', '~> 2.3.0' # Had switch to specific version to get animation effect to work.
@@ -36,7 +36,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  gem 'rspec-rails'
+  gem 'rspec-rails', '~> 3.5'
   gem 'capybara', :git => 'git://github.com/jnicklas/capybara.git'
 end
 
--- spec/requests/list_items_spec.rb
+++ spec/requests/list_items_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "ListItems", type: :request do
   describe "GET /list" do
     it "displays list items" do
-      List.create(title: "Movies", description: "My favorite movies") # Create new list item
+      List.create(title: "Movies", description: "My favorite movies") # Create new list
       get lists_path
       expect(response).to have_http_status(200)
       response.body.should include("Movies") # Checks that the text from the list item title created is included on the view page rendered.
@@ -19,10 +19,41 @@ RSpec.describe "ListItems", type: :request do
     end
   end
 
-  describe "POST /list_items" do # With this method we go through the list_items_path and does not mimic how the user would behave.
+
+
+  describe "GET /lists" do # This uses Capybara to better mimic user behaviour.
     it "create list item" do
-      post_via_redirect list_items_path, :list_item => {title: "Rower", description: "Awesome movie with Bruce Willis!", list_id: "1"}
-      response.body.should include("Rower") # Checks that the text from the list item title created is included on the view page rendered.
+      List.create(title: "Movies", description: "My favorite movies") # Had to create list here. Otherwise click_link does not work.
+      visit lists_path
+      click_on('Movies')
+
+      fill_in "list_item_title", :with => "The terminator"
+      fill_in "list_item_description", :with => "The terminator"
+      click_on "Post"
+
+      #page.should have_content("List item was successfully created.")
+      save_and_open_page
+    end
+  end
+
+  describe "GET /list_items" do # This uses Capybara to better mimic user behaviour.
+    it "create list item" do
+      visit list_items_path
+      #fill_in "new_list_item", :with => "The terminator"
+      #fill_in "new_list_item", with: 'foo bar'
+      #fill_in "Description", :with => "Cult movie about a robot."
+      #first(:link, "Edit").click
+
+      #click_link "Edit"
+      #page.should have_content("List item was successfully created.")
+      #save_and_open_page
+    end
+  end
+
+  describe "GET /list_items" do
+    it "supports js" do
+      visit list_items_path
+      click_link 'test js'
     end
   end
 
