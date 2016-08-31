6504c4e9fa3af7b4ca8c8ad22f3f36ac86b7888f
Erik <erikwjonsson@gmail.com>
2016-08-31 12:57:00 +0200

    Sessionscontroller test now works.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+  setup do
+    @user = users(:one)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test "should login" do
+    erik = users(:one)
+    post :create, name: erik.name, password: 'secret'
+    assert_redirected_to lists_url
+    assert_equal erik.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test "should fail login" do
+    erik = users(:one)
+    post :create, name: erik.name, password: 'wrong'
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
+    delete :destroy
+    assert_redirected_to login_url
   end
 
 end
