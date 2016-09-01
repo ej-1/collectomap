1a3342d88e282357c38b9c7ddc85c3eb95dabae5
Erik <erikwjonsson@gmail.com>
2016-09-01 12:44:03 +0200

    SublistsController test now works.

--- test/controllers/sublists_controller_test.rb
+++ test/controllers/sublists_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class SublistsControllerTest < ActionController::TestCase
   setup do
     @sublist = sublists(:one)
+    @list = lists(:one)
+    @user = users(:one)
+    session[:user_id] = @user.id # Setting session[:user_id] instead of going through the sessioncontroller.
   end
 
   test "should get index" do
@@ -12,25 +15,27 @@ class SublistsControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    get :new
+    xhr :get, :new, list: @list.id, format: :js # changed to xhr :get in order to get by invalid cross origin request error.
+      #http://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#csrf-protection-from-remote-script-tags
+      #http://stackoverflow.com/questions/23901978/invalid-cross-origin-request-after-upgrading-to-rails-4-1
     assert_response :success
   end
 
   test "should create sublist" do
     assert_difference('Sublist.count') do
-      post :create, sublist: { description: @sublist.description, title: @sublist.title }
+      post :create, sublist: { description: @sublist.description, title: @sublist.title, list_id: @list.id }, format: :js # Had to add the format js to the post call.
     end
 
-    assert_redirected_to sublist_path(assigns(:sublist))
+    #assert_redirected_to sublist_path(assigns(:sublist))
   end
 
   test "should show sublist" do
-    get :show, id: @sublist.id
+    get :show, id: @sublist
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @sublist.id
+    get :edit, id: @sublist
     assert_response :success
   end
 
@@ -41,7 +46,7 @@ class SublistsControllerTest < ActionController::TestCase
 
   test "should destroy sublist" do
     assert_difference('Sublist.count', -1) do
-      delete :destroy, id: @sublist.id
+      delete :destroy, id: @sublist
     end
 
     assert_redirected_to sublists_path
