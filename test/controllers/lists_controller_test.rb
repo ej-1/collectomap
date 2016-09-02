f8477be84eca5d1e134f5c78e6dbb027adc2a548
Erik <erikwjonsson@gmail.com>
2016-09-02 19:26:10 +0200

    ListsController test now checks if lists belongs to the current user.

--- test/controllers/lists_controller_test.rb
+++ test/controllers/lists_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ListsControllerTest < ActionController::TestCase
   setup do
     @list = lists(:one)
+    @list_2 = lists(:two)
     @user = users(:one)
     session[:user_id] = @user.id # Setting session[:user_id] instead of going through the sessioncontroller.
   end
@@ -48,4 +49,38 @@ class ListsControllerTest < ActionController::TestCase
 
     assert_redirected_to lists_path
   end
+
+  test "should get index sublists belonging to only user" do
+    get :index
+    assert_response :success
+
+    assigns[:lists].each do |list|
+      user = List.find(list.id).user_id
+      assert_equal user, @user.id
+    end
+  end
+
+  test "should fail to show list because list belongs to another user" do
+    get :show, id: @list_2
+    assert_redirected_to lists_url
+  end
+
+  test "should fail to edit list because list belongs to another user" do
+    get :edit, id: @list_2
+    assert_redirected_to lists_url
+  end
+
+  test "should fail to update list because list belongs to another user" do
+    patch :update, id: @list_2, sublist: { description: @list_2.description, title: @list_2.title }
+    assert_redirected_to lists_url
+  end
+
+  test "should fail to destroy list because list belongs to another user" do
+    assert_difference('List.count', 0) do
+      delete :destroy, id: @list_2
+    end
+
+    assert_redirected_to lists_url
+  end
+
 end
