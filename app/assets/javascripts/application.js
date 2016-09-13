751f66039b8d90cddcfec2e0936d38a501ba40cd
Erik <erikwjonsson@gmail.com>
2016-09-13 21:13:01 +0200

    TURBOLINKS ISSUE FIXED. Application.js was not loading sometimes or when removing 'require turbolinks' then when updating list item via edit page did not render next page, but did redirect in console. 'remote => true' in list item _form is not supposed to be used to edit page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,17 @@
 //= require jquery_ujs
 //= require jquery.remotipart
 //= require twitter/bootstrap
-//= require turbolinks
+// removed '//=require turbolinks' because I had to reload the page sometimes to make application.js load.
+// Read about it here in comment nr 5 - http://stackoverflow.com/questions/17881384/jquery-gets-loaded-only-on-page-refresh-in-rails-4-application
+// Also had to "split up" form for list items by putting the code directly in edit view withouth remote=> true which was making the page redirect to the right page in the console, but not render the page.
+// The remaing form is rendered when creating a new list item and uses remote => true to not reload the page.
 //= require_tree .
 
-$('document').ready(function(){
+$(document).ready(function(){
 
   $( ".clear-adress-btn" ).click(function() {
               document.getElementById("list_item_adress").value = ''; // Send coordinates to adress field.
-              document.getElementById("adress_info").innerHTML = 'Click on map to add adress marker'; // Send coordinates to adress field.
+              document.getElementById("adress_info").innerHTML = 'Click on map to add or change adress marker'; // Send coordinates to adress field.
   });
 
 });
