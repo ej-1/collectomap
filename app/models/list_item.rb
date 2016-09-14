0f7c3d2ae5271fbe4b628e6ab931c0c87f6d5091
Erik <erikwjonsson@gmail.com>
2016-09-14 15:14:10 +0200

    Removed only_one_image method that checked that not both image file and image url can be submitted at the same time for list item. REASON: Carrierwave never has image_url for file empty when submitting remote_image_url.

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,7 +1,7 @@
 class ListItem < ActiveRecord::Base
 	belongs_to :lists
 	validates :title, :description ,presence: true # adress is optional
-	validate :check_image_url, :if => :only_one_image # Check that url for image is valid if it is only url or image given.
+	validate :check_image_url # Check that url for image is valid.
 	validates :description, :uniqueness => {:scope => [:title, :description]} # Add user id to check it unique to user.
 	mount_uploader :image, ImageUploader
 
@@ -14,15 +14,6 @@ class ListItem < ActiveRecord::Base
 	  if self.remote_image_url.present? && self.remote_image_url.end_with?(".jpg", ".jpeg", ".pgn.") 
 	  elsif self.remote_image_url.present?
 	  	errors.add(:string, "Not valid image url. Make sure it is .jpg, .gif or .pgn.")
-	  end
-	end
-
-	def only_one_image
-	  if (!self.remote_image_url.blank?) && (!self.image_url.blank?)
-	  	errors.add(:string, "Cannot submit both file image and image url.")
-	  	false
-	  else
-	  	true
 	  end
 	end
 
